f7c231c17f8bdd39cdf43562086528987873c14a
Filippo Italiano <filippoitaliano.work@gmail.com>
2022-04-30 15:08:28 +0200

    Binded settings and controls

--- logic.js
+++ logic.js
@@ -1,16 +1,67 @@
 
-function startTimerNeedleMovement(timerMinutes = 1) {
-  let timerSeconds = timerMinutes * 60;
-  const needles = document.getElementsByClassName('analog-timer-needle');
-  let timerDegRotation = 0;
-  const intervalId = setInterval(() => {
+let timerDuration = 25;
+let breakDuration = 5;
+let timerTimes = 3;
+
+let timerIntervalId = null;
+
+let analogTimerNeedle = null;
+let analogTimerDegRotation = 0;
+
+function startTimer() {
+  let timerSeconds = timerDuration * 60;
+  timerIntervalId = setInterval(() => {
     if (timerSeconds === 0) {
-      clearInterval(intervalId);
-      timerDegRotation = 0;
+      clearInterval(timerIntervalId);
+      timerIntervalId = null;
+      resetAnalogTimerNeedle()
     } else {
       timerSeconds -= 1;
-      timerDegRotation = (timerDegRotation + 6) % 360;
-      needles[0].style.transform = `rotate(${timerDegRotation}deg)`;
+      rotateAnalogTimerNeedle();
     }
   }, 1000);
 }
+
+function abortTimer() {
+  clearInterval(timerIntervalId);
+  timerIntervalId = null;
+  resetAnalogTimerNeedle();
+}
+
+function rotateAnalogTimerNeedle(degRotation = null) {
+  if (degRotation != null) {
+    analogTimerDegRotation = 0;
+  } else {
+    analogTimerDegRotation = (analogTimerDegRotation + 6) % 360;
+  }
+  analogTimerNeedle.style.transform = `rotate(${analogTimerDegRotation}deg)`;
+}
+
+function resetAnalogTimerNeedle() {
+  rotateAnalogTimerNeedle(0)
+}
+
+function bindSettingsToForm() {
+  const timerDurationInput = document.getElementById('timer-duration-input');
+  timerDurationInput.value = timerDuration;
+  timerDurationInput.onchange = (event) => { timerDuration = event.target.value; };
+
+  const breakDurationInput = document.getElementById('break-duration-input');
+  breakDurationInput.value = breakDuration;
+  breakDurationInput.onchange = (event) => { breakDuration = event.target.value; };
+
+  const timerTimesInput = document.getElementById('timer-times-input');
+  timerTimesInput.value = timerTimes;
+  timerTimesInput.onchange = (event) => { timerTimes = event.target.value; };
+}
+
+function getAnalogTimerNeedleRef() {
+  analogTimerNeedle = document.getElementById('analog-timer-needle');
+}
+
+function appInit() {
+  bindSettingsToForm();
+  getAnalogTimerNeedleRef();
+}
+
+document.addEventListener('DOMContentLoaded', appInit, false);
